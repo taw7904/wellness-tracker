43e02f666b3146f2826ba5a8ac91811e32137e01
taw7904 <taw7904@rit.edu>
2020-10-02 21:06:19 -0400

    adding rest implementation and animated gifs

--- src/htmlResponses.js
+++ src/htmlResponses.js
@@ -3,6 +3,10 @@ const fs = require('fs');
 const index = fs.readFileSync(`${__dirname}/../client/client.html`);
 const style = fs.readFileSync(`${__dirname}/../client/style.css`);
 const editIcon = fs.readFileSync(`${__dirname}/../images/edit.png`);
+const sleepIcon = fs.readFileSync(`${__dirname}/../images/sleep.gif`);
+const waterIcon = fs.readFileSync(`${__dirname}/../images/water.gif`);
+const foodIcon = fs.readFileSync(`${__dirname}/../images/food.gif`);
+const exerciseIcon = fs.readFileSync(`${__dirname}/../images/exercise.gif`);
 
 const getIndex = (request, response) => {
   response.writeHead(200, { 'Content-Type': 'text/html' });
@@ -22,6 +26,34 @@ const getEditIcon = (request, response) => {
   response.end();
 };
 
+const getSleepIcon = (request, response) => {
+  response.writeHead(200, { 'Content-Tpye': 'image/gif' });
+  response.write(sleepIcon);
+  response.end();
+};
+
+const getFoodIcon = (request, response) => {
+  response.writeHead(200, { 'Content-Tpye': 'image/gif' });
+  response.write(foodIcon);
+  response.end();
+};
+
+const getExerciseIcon = (request, response) => {
+  response.writeHead(200, { 'Content-Tpye': 'image/gif' });
+  response.write(exerciseIcon);
+  response.end();
+};
+
+const getWaterIcon = (request, response) => {
+  response.writeHead(200, { 'Content-Tpye': 'image/gif' });
+  response.write(waterIcon);
+  response.end();
+};
+
 module.exports.getIndex = getIndex;
 module.exports.getStyle = getStyle;
 module.exports.getEditIcon = getEditIcon;
+module.exports.getSleepIcon = getSleepIcon;
+module.exports.getFoodIcon = getFoodIcon;
+module.exports.getExerciseIcon = getExerciseIcon;
+module.exports.getWaterIcon = getWaterIcon;
